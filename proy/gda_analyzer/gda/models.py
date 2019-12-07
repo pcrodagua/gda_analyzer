2780fa82a01ea9c32c1f3fd1786e3d1ffc53a7c2
Pablo Rodriguez <is.pab.ces@gmail.com>
2019-12-06 21:10:29 -0600

    [New] Se agregó el proyecto GDA y su app
    [New] Se agregó el proyecto GDA y su app

---
+++ proy/gda_analyzer/gda/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Producto(models.Model):
+    nombre = models.CharField(unique=False)
+    grasa_saturada = models.IntegerField()
+    otras_grasas  = models.IntegerField()
+    azucar_total = models.IntegerField()
+    sodio = models.IntegerField()
+    energia = models.IntegerField()
+    energia_pporcion = models.IntegerField()
+    porcion = models.IntegerField()
---
+++ proy/gda_analyzer/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ospcx/GitProjects/gda_analyzer/proy/gda_analyzer/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
